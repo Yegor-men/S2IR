a01113b3032b0e08e9e08d7a4255b979a24b84b4
Yegor Men <yegormenovchshikov@gmail.com>
2025-09-11 16:02:00 +1000

    created an idiotic sampler with the concept of noise - prediction = clean image. looking back it's stupid, because that would mean that the prediction the model produces is noise - image, meaning that internally it can already detect both the noise and the image and subtract them. if it can do either of these tasks, then theres no need for this approach, so i suppose i'll look back to this later maybe if at all

---
+++ idiot_sampler.py
@@ -0,0 +1,77 @@
+# ======================================================================================================================
+import torch
+from modules.alpha_bar import alpha_bar_cosine
+from modules.relative_positional_conditioning import relative_positional_conditioning
+from modules.global_embed import global_embed
+from modules.render_image import render_image
+
+B, C, H, W = 9, 1, 28, 28
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+from modules.s2ir import SIIR
+
+model = SIIR(
+	c_channels=1,
+	d_channels=64,
+	num_heads=4,
+	num_blocks=4,
+	t_dim=16,
+	text_embed_dim=10,
+	pos_embed_dim=2,
+	dropout_p_ffw=0.33,
+	dropout_p_axial=0.15,
+	dropout_p_cross=0.15,
+)
+
+from save_load_model import load_checkpoint_into
+
+model = load_checkpoint_into(model, "models/s2ir_20250910_222236.pt", "cuda")
+model.to(device)
+model.eval()
+
+label = torch.zeros(10)
+label[0] = 1.0
+labels = label.unsqueeze(0).expand(B, -1).to(device)
+
+noisy_image = torch.randn(B, C, H, W).to(device)
+positive_text_conditioning = global_embed(labels, H, W).to(device)
+# zero_text_conditioning = torch.zeros_like(positive_text_conditioning).to(device)
+position_conditioning = relative_positional_conditioning(noisy_image).to(device)
+render_image(((noisy_image + 1) / 2).clamp(min=0.0, max=1))
+# ======================================================================================================================
+
+num_diffusion_steps = 10
+diffusion_per_step = torch.arange(1, num_diffusion_steps + 1, 1)
+diffusion_per_step = 1 / diffusion_per_step
+diffusion_per_step = diffusion_per_step / torch.sum(diffusion_per_step)
+print(diffusion_per_step.numel())
+
+mix_percent = 1
+alpha_bar = torch.zeros(B).to(device)
+
+with torch.no_grad():
+	for i in range(num_diffusion_steps):
+		diffusion_amount = diffusion_per_step[i].item()
+
+		positive = model(
+			noisy_image,
+			positive_text_conditioning,
+			position_conditioning,
+			alpha_bar
+		)
+
+		# negative = model(
+		# 	noisy_image,
+		# 	zero_text_conditioning,
+		# 	position_conditioning,
+		# 	alpha_bar
+		# )
+
+		# final_prediction = positive * mix_percent + negative * (1 - mix_percent)
+
+		noisy_image -= diffusion_amount * positive
+
+		alpha_bar += diffusion_amount
+
+		render_image(((noisy_image + 1) / 2).clamp(min=0.0, max=1))
