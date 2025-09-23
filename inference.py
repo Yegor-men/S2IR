Fixed SNR loss scaling, although the exact method is still finnicky

inference.py
@@ -4,7 +4,6 @@ from modules.alpha_bar import alpha_bar_cosine
 from modules.render_image import render_image
 
 B, C, H, W = 100, 1, 28, 28
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 from modules.s2ir import SIIR
@@ -25,7 +24,7 @@ model = SIIR(
 
 from save_load_model import load_checkpoint_into
 
-model = load_checkpoint_into(model, "models/bar.pt", "cuda")
+model = load_checkpoint_into(model, "models/foo_sqrt_20_039.pt", "cuda")
 model.to(device)
 model.eval()
 

modules/alpha_bar.py
@@ -3,7 +3,6 @@ import math
 
 
 def alpha_bar_cosine(t: torch.Tensor, s: float = 0.008) -> torch.Tensor:
-	eps = 1e-8
 	factor = (t + s) / (1.0 + s)
-	alpha_bar = torch.cos(factor * math.pi / 2.0).clamp(min=eps, max=1 - eps) ** 2
+	alpha_bar = torch.cos(factor * math.pi / 2.0).clamp(min=0.0, max=1.0) ** 2
 	return alpha_bar

train.py
@@ -60,7 +60,7 @@ model = SIIR(
 
 # from save_load_model import load_checkpoint_into
 #
-# model = load_checkpoint_into(model, "models/foo.pt", "cuda")
+# model = load_checkpoint_into(model, "models/foo_ln_40.pt", "cuda")
 # model.to(device)
 # model.eval()
 
@@ -125,7 +125,7 @@ train_dloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_dloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 peak_lr = 1e-3
-final_lr = 1e-4
+final_lr = 1e-5
 total_steps = num_epochs * len(train_dloader)
 warmup_steps = len(train_dloader)
 
@@ -172,8 +172,8 @@ for E in range(num_epochs):
 				text_cond = text_cond.to(device)
 				alpha_bar = alpha_bar.to(device)
 
-				snr = alpha_bar / (1 - alpha_bar)
-				weight = (1 / (snr + 1e-6)).clamp_(max=100.0)
+				weight = torch.log(1 + (1 - alpha_bar) / (alpha_bar + 1e-2))
+				weight /= weight.mean()
 
 			predicted_noise = model(noisy_image, text_cond, alpha_bar)
 			# loss = nn.functional.mse_loss(predicted_noise, expected_output)
@@ -216,8 +216,8 @@ for E in range(num_epochs):
 				text_cond = text_cond.to(device)
 				alpha_bar = alpha_bar.to(device)
 
-				snr = alpha_bar / (1 - alpha_bar)
-				weight = (1 / (snr + 1e-6)).clamp_(max=100.0)
+				weight = torch.log(1 + (1 - alpha_bar) / (alpha_bar + 1e-2))
+				weight /= weight.mean()
 
 				predicted_noise = ema_model(noisy_image, text_cond, alpha_bar)
 				# loss = nn.functional.mse_loss(predicted_noise, expected_output)
@@ -271,8 +271,8 @@ for E in range(num_epochs):
 				text_cond = text_cond.to(device)
 				alpha_bar = alpha_bar.to(device)
 
-				snr = alpha_bar / (1 - alpha_bar)
-				weight = (1 / (snr + 1e-6)).clamp_(max=100.0)
+				weight = torch.log(1 + (1 - alpha_bar) / (alpha_bar + 1e-2))
+				weight /= weight.mean()
 
 				predicted_noise = ema_model(noisy_image, text_cond, alpha_bar)
 				mse_loss = nn.functional.mse_loss(predicted_noise, expected_output)